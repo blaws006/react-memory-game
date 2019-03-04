9f2a9e5968f75704b21c930b70bc72eae9e2e1ba
blaws006 <blaws006@gmail.com>
2019-03-03 19:38:57 -0500

    Images randomize now.

--- my-app/src/components/Main/Main.js
+++ my-app/src/components/Main/Main.js
@@ -12,68 +12,154 @@ import drake from "../img/NathanDrake-U4.jpg";
 import ryu from "../img/Ryu_TvC.png";
 import sonic from "../img/sonic.jpg";
 
-// Create img tags
-//
+// Create a list
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isClicked: false};
+    this.state = {
+      isClicked: false,
+      list: [
+        cloud,
+        lara,
+        dk,
+        aloy,
+        mario,
+        dante,
+        kratos,
+        link,
+        samus,
+        drake,
+        ryu,
+        sonic
+      ]
+    };
   }
+  shuffleArray = () => {
+    var list = this.state.list;
+    for (var i = list.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = list[i];
+      list[i] = list[j];
+      list[j] = temp;
+    }
+    this.setState(state => ({
+      list: state.list
+    }))
+    console.log(list)
+  };
 
-  
-  handleClick = () =>  {
-    console.log('Click');
+  handleClick = () => {
+    console.log("Click");
     this.setState({
       isClicked: true
-    })
-    console.log(this.props)
-  }
+    });
+  };
 
   render() {
     return (
       <div className="container content">
         <div className="row">
-          <div className="col s12 m3 l3 center">
-            <img src={cloud} alt={cloud} onClick={this.handleClick} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[0]}
+              alt={this.state.list[0]}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={lara} alt={lara} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[1]}
+              alt={lara}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={dk} alt={dk} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[2]}
+              alt={dk}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={aloy} alt={aloy} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[3]}
+              alt={aloy}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
         </div>
         <div className="row">
-          <div className="col s12 m3 l3 center">
-            <img src={mario} alt={mario} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[4]}
+              alt={mario}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={dante} alt={dante} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[5]}
+              alt={dante}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={kratos} alt={kratos} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[6]}
+              alt={kratos}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={link} alt={link} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[7]}
+              alt={link}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
         </div>
 
         <div className="row">
-          <div className="col s12 m3 l3 center">
-            <img src={samus} alt={samus} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[8]}
+              alt={samus}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={drake} alt={drake} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[9]}
+              alt={drake}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={ryu} alt={ryu} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[10]}
+              alt={ryu}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
-          <div className="col s12 m3 l3 center">
-            <img src={sonic} alt={sonic} />
+          <div className="col s3 m3 l3 center">
+            <img
+              src={this.state.list[11]}
+              alt={sonic}
+              className="responsive-img"
+              onClick={this.shuffleArray}
+            />
           </div>
         </div>
       </div>
