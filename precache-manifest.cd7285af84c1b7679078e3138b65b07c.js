622a0d047495e4e361355d55557f8bd34b69ceee
blaws006 <blaws006@gmail.com>
2019-03-06 19:42:19 -0500

    Updates

---
+++ precache-manifest.cd7285af84c1b7679078e3138b65b07c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7c6a38fb173b4d55885",
+    "url": "/react-memory-game/static/css/main.f4a877dd.chunk.css"
+  },
+  {
+    "revision": "f7c6a38fb173b4d55885",
+    "url": "/react-memory-game/static/js/main.f7c6a38f.chunk.js"
+  },
+  {
+    "revision": "a2ed142be53bff71048f",
+    "url": "/react-memory-game/static/js/runtime~main.a2ed142b.js"
+  },
+  {
+    "revision": "116357c7775f271151fb",
+    "url": "/react-memory-game/static/js/2.116357c7.chunk.js"
+  },
+  {
+    "revision": "1ee09ff85c868e415aa2bef8d6078e88",
+    "url": "/react-memory-game/static/media/controller-game-hands-807330.1ee09ff8.jpg"
+  },
+  {
+    "revision": "a48f5637b96f2bcfca78081974d4f3f3",
+    "url": "/react-memory-game/static/media/220px-Cloud_Strife.a48f5637.png"
+  },
+  {
+    "revision": "e5abfda88146561ea382e0db3915653d",
+    "url": "/react-memory-game/static/media/220px-Lara_Croft_(2013).e5abfda8.png"
+  },
+  {
+    "revision": "05afd3d15b4887d1c2f5c1f0d2ea5f81",
+    "url": "/react-memory-game/static/media/250px-SuperMarioParty_DonkeyKong.05afd3d1.png"
+  },
+  {
+    "revision": "c9f0356e9bd7fed19c1542fb39ba5b43",
+    "url": "/react-memory-game/static/media/aloy.c9f0356e.jpg"
+  },
+  {
+    "revision": "65c77ff932a8f7512bbc58bb26f8a9cf",
+    "url": "/react-memory-game/static/media/char-mario-action.65c77ff9.png"
+  },
+  {
+    "revision": "4cf73a245dd37706d70ea891d8df735b",
+    "url": "/react-memory-game/static/media/dante.4cf73a24.jpg"
+  },
+  {
+    "revision": "5b16011358308785d5b455d43ea0f427",
+    "url": "/react-memory-game/static/media/kratos.5b160113.jpg"
+  },
+  {
+    "revision": "bb221e95667a139b45bd3bcfa9c6f775",
+    "url": "/react-memory-game/static/media/lozbotw-link-and-bow.bb221e95.jpg"
+  },
+  {
+    "revision": "b70c631df202370203d21cead801cb47",
+    "url": "/react-memory-game/static/media/Metroidprime3_1.b70c631d.png"
+  },
+  {
+    "revision": "37e184e2daf47d53bf307f89be2d31f0",
+    "url": "/react-memory-game/static/media/NathanDrake-U4.37e184e2.jpg"
+  },
+  {
+    "revision": "4afbb0beacd3437bca445c67ade65e5b",
+    "url": "/react-memory-game/static/media/Ryu_TvC.4afbb0be.png"
+  },
+  {
+    "revision": "52428ade7af856d2756dfd9f99ef194d",
+    "url": "/react-memory-game/static/media/sonic.52428ade.jpg"
+  },
+  {
+    "revision": "ff3d1243e46ac4fadb4ce8dc65ea7c27",
+    "url": "/react-memory-game/index.html"
+  }
+];
